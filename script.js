a4783be99d78200bddecc9ab9d57b1ef1ea3cd8e
SymPiracha <sym.piracha@gmail.com>
2020-07-25 12:23:31 +0500

    loading screen implemented

--- script.js
+++ script.js
@@ -3,28 +3,45 @@ const quote = document.getElementById('quote');
 const author = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('newQuote');
+const loader = document.getElementById('loader');
 
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote From API
 async function getQuote() {
+    showLoadingSpinner();
     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
+
         // If author is blank, replace with 'unknown'
         if (data.quoteAuthor === '') {
             author.innerText = "Unknown";
         } else {
             author.innerText = data.quoteAuthor;
         }
+
         // Reduce font size if char > 120
-        if (data.quoteText.length > 50) {
+        if (data.quoteText.length > 120) {
             quote.classList.add('long-quote');
         } else {
             quote.classList.remove('long-quote');
         }
         quote.innerText = data.quoteText;
+
+        removeLoadingSpinner();
     }
     catch(error) {
 
@@ -42,8 +59,6 @@ function tweet() {
 // Event Listeners
 twitterBtn.addEventListener('click', tweet);
 newQuoteBtn.addEventListener('click', getQuote);
-
-
 
 //On Load
 getQuote();
