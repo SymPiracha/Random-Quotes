75645f71c5d9a0ebb7851aff6e2237289695cf56
SymPiracha <sym.piracha@gmail.com>
2020-09-05 18:40:43 -0400

    fixed infinite loading screen for first load

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function removeLoadingSpinner() {
 
 // Get Quote From API
 async function getQuote() {
-    showLoadingSpinner();
+    // showLoadingSpinner();
     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
